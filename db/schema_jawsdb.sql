4a98061afe73a65218ba31f96bb1170619788451
Margaret Choi <margaret.j.li@gmail.com>
2017-07-25 00:19:17 -0700

    Styling updates to image

--- db/schema_jawsdb.sql
+++ db/schema_jawsdb.sql
@@ -1,16 +1,17 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE igekhnet9v5pczbt;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP NOT NULL, 
-	createdAt TIMESTAMP NOT NULL,
+	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
 INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+
+SELECT * from burgers;
